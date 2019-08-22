d661ffb75ad312901c4f1546409882723cef2789
Nick Wang <nickyiliwang@gmail.com>
2019-08-22 08:13:41 -0400

    Updates

---
+++ precache-manifest.1afbe3efc899a08f5e99409bc4f4228f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d38c07893666fd79f54fad9222b0783c",
+    "url": "/taskMaster-deploy/index.html"
+  },
+  {
+    "revision": "79180df02952046019ff",
+    "url": "/taskMaster-deploy/static/js/2.279cde98.chunk.js"
+  },
+  {
+    "revision": "5eeae7df466ac9b376d9",
+    "url": "/taskMaster-deploy/static/js/main.078fc31f.chunk.js"
+  },
+  {
+    "revision": "48a82bb7eace144283e7",
+    "url": "/taskMaster-deploy/static/js/runtime~main.e4e8fc6b.js"
+  }
+]);
